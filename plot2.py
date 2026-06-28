d531035ea77b2f21a6ab3bfcc0d70a58ba395448
aniii03 <ananyamandal1307@gmail.com>
2026-06-28 19:03:13 +0530

    Update plot2.py

--- plot2.py
+++ plot2.py
@@ -76,7 +76,7 @@ def make_animation(results, alpha):
                         color='red',
                         width=2
                     ),
-                    opacity=0.4,
+                    opacity=0.2,
                     showlegend=False,
                     hoverinfo='skip'
                 )
